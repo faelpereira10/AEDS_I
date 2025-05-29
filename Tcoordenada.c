ba496073d5d12228b9a7fa62f2cb982f6afdb58c
Rafael Pereira <rafael.contatopereira@gmail.com>
2025-05-29 16:34:54 -0300

    update

--- Tcoordenada.c
+++ Tcoordenada.c
@@ -14,5 +14,5 @@ Tcoordenada criar_coordenada(int linha, int coluna){
 
 int verificar_coordenada(Tcoordenada coordenada, Tmatriz *matriz){
 
-    return coordenada.coluna = > 0 && coordenada.coluna < matriz->colunas && coordenada.linha = > 0 && coordenada.linha < matriz->linhas;
+    return coordenada.coluna >= 0 && coordenada.coluna < matriz->colunas && coordenada.linha >=  0 && coordenada.linha < matriz->linhas;
 }
